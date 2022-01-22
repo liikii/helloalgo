269d66c7732b89b6e44984a4403584266a380642
liikii <609957891@qq.com>
2022-01-22 22:26:11 +0800

    rename 01 02, serial to end.

---
+++ binarytree01.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+simple binary tree example.
+from tutorials point.
+"""
+
+
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print(self.data)
+        if self.right:
+            self.right.print_tree()
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    # recursion
+                    self.left.insert(data)
+            # TODO: ignore equality.
+            # symmetry.
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            # root
+            self.data = Node(data)
